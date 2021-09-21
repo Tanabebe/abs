fdea99ccef6471c890837dd665c157c3e1a443a9
Tanabebe <nob@tanabebe.tech>
2021-09-21 22:21:53 +0900

    answer a

---
+++ src/bin/abc049c.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+
+fn main() {
+    input!{
+        a: String,
+        b: String,
+        c: String,
+        s: String,
+    }
+
+    println!("{} {} {} {}", a, b, c, s);
+}
